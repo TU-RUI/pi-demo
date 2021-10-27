2ece9f6cd5848da906eab2c8d70a10201447ef04
turui <turui@kuaishou.com>
2021-10-27 19:51:36 +0800

    init

---
+++ src/test/java/com/turui/pi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turui.pi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
