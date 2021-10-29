4d865d5c8a593f1ed0e3f4758a3838c75bca18b2
turui <turui@kuaishou.com>
2021-10-29 10:15:38 +0800

    1

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 ##idea插件  
 ide-eval-resetter
 
-PI4J
-https://pi4j.com/1.3/
-https://pi4j.com/1.3/usage.html
 
-pi4j-v2
-https://pi4j.com/getting-started/minimal-example-application/
-https://github.com/Pi4J/pi4j-example-minimal/blob/main/src/main/java/com/pi4j/example/MinimalExample.java
 
-JAVA-PI4J 树莓派利用PCA9685操控舵机
+---
+## 参考资料
+
+PI4J-1.3  
+https://pi4j.com/1.3/  
+https://pi4j.com/1.3/usage.html  
+
+pi4j-v2  
+https://pi4j.com/getting-started/minimal-example-application/  
+https://github.com/Pi4J/pi4j-example-minimal/blob/main/src/main/java/com/pi4j/example/MinimalExample.java  
+
+JAVA-PI4J 树莓派利用PCA9685操控舵机  
 https://blog.csdn.net/m0_46552807/article/details/107641895
--- src/main/java/com/turui/pi/demo/PiExample.java
+++ src/main/java/com/turui/pi/demo/PiExample.java
@@ -1,10 +1,20 @@
 package com.turui.pi.demo;
 
+import java.util.concurrent.TimeUnit;
+
 import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
 import com.pi4j.io.gpio.digital.DigitalOutput;
-import com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder;
 import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.plugin.mock.platform.MockPlatform;
+import com.pi4j.plugin.mock.provider.gpio.analog.MockAnalogInputProvider;
+import com.pi4j.plugin.mock.provider.gpio.analog.MockAnalogOutputProvider;
+import com.pi4j.plugin.mock.provider.gpio.digital.MockDigitalInputProvider;
+import com.pi4j.plugin.mock.provider.gpio.digital.MockDigitalOutputProvider;
+import com.pi4j.plugin.mock.provider.i2c.MockI2CProvider;
+import com.pi4j.plugin.mock.provider.pwm.MockPwmProvider;
+import com.pi4j.plugin.mock.provider.serial.MockSerialProvider;
+import com.pi4j.plugin.mock.provider.spi.MockSpiProvider;
 import com.pi4j.util.Console;
 
 /**
@@ -15,34 +25,49 @@ public class PiExample {
     /** PIN 15 = BCM 22 */
     private static final int PIN_LED = 22;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         final Console console = new Console();
         console.title("<-- The Pi4J Project -->", "Minimal Example project");
-        Context pi4j = Pi4J.newAutoContext();
+        //自动生成，context需要全局唯一
+//        Context pi4j = Pi4J.newAutoContext();
+        //测试用mock对象
+        Context pi4j = Pi4J.newContextBuilder()
+                .add(new MockPlatform())
+                .add(MockAnalogInputProvider.newInstance(),
+                        MockAnalogOutputProvider.newInstance(),
+                        MockSpiProvider.newInstance(),
+                        MockPwmProvider.newInstance(),
+                        MockSerialProvider.newInstance(),
+                        MockI2CProvider.newInstance(),
+                        MockDigitalInputProvider.newInstance(),
+                        MockDigitalOutputProvider.newInstance())
+                .build();
+        console.title("<-- Pi4J Context created -->");
         PrintInfo.printLoadedPlatforms(console, pi4j);
         PrintInfo.printDefaultPlatform(console, pi4j);
         PrintInfo.printProviders(console, pi4j);
 
-        // Here we will create I/O interfaces for a (GPIO) digital output
-        // and input pin. We define the 'provider' to use PiGpio to control
-        // the GPIO.
-        DigitalOutputConfigBuilder ledConfig = DigitalOutput.newConfigBuilder(pi4j)
-                .id("led")
-                .name("LED Flasher")
-                .address(PIN_LED)
-                .shutdown(DigitalState.LOW)
-                .initial(DigitalState.LOW)
-                .provider("pigpio-digital-output");
-        DigitalOutput led = pi4j.create(ledConfig);
+        DigitalOutput digitalOutput = pi4j.dout().create(PIN_LED);
+        digitalOutput.addListener(System.out::println);
+        //闪烁
+        digitalOutput.state(DigitalState.HIGH)
+                .state(DigitalState.LOW)
+                .state(DigitalState.HIGH)
+                .state(DigitalState.LOW);
+        //
+        digitalOutput.toggle()
+                .toggle()
+                .toggle();
+        digitalOutput.low().high().low().high();
 
-        for (int i = 0; i < 10; i++) {
-            led.high();
-            Thread.sleep(1000);
-            led.low();
-        }
-        console.println("led end");
+        console.println("CURRENT DIGITAL OUTPUT [" + digitalOutput + "] STATE IS [" + digitalOutput.state() + "]");
+        console.println("PULSING OUTPUT STATE TO HIGH FOR 3 SECONDS");
+        digitalOutput.pulse(3, TimeUnit.SECONDS, DigitalState.HIGH);
+        console.println("PULSING OUTPUT STATE COMPLETE");
 
         pi4j.shutdown();
     }
+
+
 
 }
