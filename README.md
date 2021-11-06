4dd12c7f985746f2a192479a90e098b94cd21fd0
TU-RUI <991642404@qq.com>
2021-11-07 00:39:41 +0800

    1

--- README.md
+++ README.md
@@ -15,4 +15,7 @@ https://pi4j.com/getting-started/minimal-example-application/
 https://github.com/Pi4J/pi4j-example-minimal/blob/main/src/main/java/com/pi4j/example/MinimalExample.java  
 
 JAVA-PI4J 树莓派利用PCA9685操控舵机  
-https://blog.csdn.net/m0_46552807/article/details/107641895
+https://blog.csdn.net/m0_46552807/article/details/107641895   
+
+PCA9685：I2C转16路PWM，助力你的系统   
+https://zhuanlan.zhihu.com/p/67336644
--- src/main/java/com/turui/pi/demo/PiI2CExample.java
+++ src/main/java/com/turui/pi/demo/PiI2CExample.java
@@ -14,21 +14,63 @@ import com.pi4j.util.Console;
  * Created on 2021-10-27
  */
 public class PiI2CExample {
-    /**
-     * I2C设备地址，查看方式：sudo i2cdetect -y 1
-     */
-    private static final int PCA9685_INPUT = 0x40;
-    /**
-     * I2C设备输出通道，0-16
-     */
-    private static final byte PCA9685_OUTPUT_1 = 0x0f;
-    private static final byte PCA9685_OUTPUT_2 = 0x0e;
+    /**PCA9685参考：https://zhuanlan.zhihu.com/p/67336644 */
+
+    //I2C设备地址，查看方式：sudo i2cdetect -y 1
+    private static final int PCA9685_ADDR = 0x40;
+    //PWM频率25MHz
+    private static final float CLOCK_FREQ = 25000000F;
+    private static final byte MODE1 = 0x00;
+    private static final byte MODE2 = 0x01;
+    private static final int PRE_SCALE = 0xFE;
+    private static final byte LED0_ON_L = 0x06;
+    private static final byte LED0_ON_H = 0x07;
+    private static final byte LED0_OFF_L = 0x08;
+    private static final byte LED0_OFF_H = 0x09;
+    private static final int ALLLED_ON_L = 0xFA;
+    private static final int ALLLED_ON_H = 0xFB;
+    private static final int ALLLED_OFF_L = 0xFC;
+    private static final int ALLLED_OFF_H = 0xFD;
+
+    private static final byte SHIFT = 4;
+    //舵机通道14,15
+    private static final int CHANNEL_1 = 14;
+    private static final int CHANNEL_2 = 15;
+
+    private static Context pi4j;
+
+
+    //初始化
+    private static void initPCA9685(I2C pca9685) throws InterruptedException {
+        pca9685.writeRegister(MODE1, 0X00);
+        pca9685.writeRegister(MODE2, 0X04); //可删?
+        int preScale = (int) Math.floor((CLOCK_FREQ / 4096) / 50 - 0.5);
+
+        int oldMode = pca9685.readRegister(MODE1);
+        //sleep
+        int newMode = (oldMode & 0x7F) | 0x10;
+        pca9685.writeRegister(MODE1, newMode);
+
+        pca9685.writeRegister(MODE1, newMode);
+        //设置频率
+        pca9685.writeRegister(PRE_SCALE, preScale);
+        pca9685.writeRegister(MODE1, oldMode);
+        Thread.sleep(500);
+        pca9685.writeRegister(MODE1, oldMode | 0xa1); //0x80?
+
+        //初始化舵机
+        pca9685.writeRegister(LED0_ON_L + SHIFT * CHANNEL_1, 0);
+        pca9685.writeRegister(LED0_ON_H + SHIFT * CHANNEL_1, 0);
+        pca9685.writeRegister(LED0_ON_L + SHIFT * CHANNEL_2, 0);
+        pca9685.writeRegister(LED0_ON_H + SHIFT * CHANNEL_2, 0);
+    }
+
 
     public static void main(String[] args) throws InterruptedException {
         final Console console = new Console();
         console.title("<-- The Pi4J Project -->", "Minimal Example project");
         //自动生成，context需要全局唯一
-        Context pi4j = Pi4J.newAutoContext();
+        pi4j = Pi4J.newAutoContext();
 
         console.title("<-- Pi4J Context created -->");
         PrintInfo.printLoadedPlatforms(console, pi4j);
@@ -39,27 +81,15 @@ public class PiI2CExample {
         console.println("----  i2c start  ----");
         I2CProvider i2CProvider = pi4j.provider("i2c");
         //查看i2c设备地址：sudo i2cdetect -y 1
-        I2CConfig i2cConfig = I2C.newConfigBuilder(pi4j).id("PCA9685").bus(1).device(PCA9685_INPUT).build();
+        I2CConfig i2cConfig = I2C.newConfigBuilder(pi4j)
+                .id("PCA9685")
+                .bus(1)
+                .device(PCA9685_ADDR)
+                .build();
         try (I2C pca9685 = i2CProvider.create(i2cConfig)) {
-            int config1 = pca9685.readRegister(PCA9685_OUTPUT_1);
-            int config2 = pca9685.readRegister(PCA9685_OUTPUT_2);
-            if (config1 < 0) {
-                throw  new IllegalStateException(
-                        "Failed to read configuration from address 0x" + String.format("%02x", PCA9685_OUTPUT_1));
-            }
-            console.println("config1 is succ! state=" + config1);
-            if (config2 < 0) {
-                throw  new IllegalStateException(
-                        "Failed to read configuration from address 0x" + String.format("%02x", PCA9685_OUTPUT_2));
-            }
-            console.println("config2 is succ! state=" + config2);
-            byte state1 = 0x00;
-            pca9685.writeRegister(PCA9685_OUTPUT_1, state1);
-            pca9685.writeRegister(PCA9685_OUTPUT_2, state1);
-            setPin(state1, 0, pca9685, PCA9685_OUTPUT_1, true);
-            setPin(state1, 1, pca9685, PCA9685_OUTPUT_1, true);
+            //初始化
+            initPCA9685(pca9685);
         }
-
 
         console.println("----  i2c end  ----");
 
